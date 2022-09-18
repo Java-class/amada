be957cac6544df18d4146c2cf2124919bdc83974
Mostafa Anbarmoo <mostafa.anbarmoo@gmail.com>
2022-09-18 17:29:46 +0430

    create project structure

---
+++ src/main/java/ir/javaclass/amada/AmadaApplication.java
@@ -0,0 +1,13 @@
+package ir.javaclass.amada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmadaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmadaApplication.class, args);
+    }
+
+}
